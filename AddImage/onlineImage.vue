7cfb0e7104096fc253eb16296d06e2a7c4402f05
zhumengtuo <zhumengtuo@MTMAC.local>
2019-03-11 13:58:31 +0800

    修复在线插入

--- AddImage/onlineImage.vue
+++ AddImage/onlineImage.vue
@@ -70,16 +70,13 @@ export default {
         }
     },
     checkURL(URL) {
+      console.log(URL);
       var str = URL;
       //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
       //下面的代码中应用了转义字符"\"输出一个字符"/"
-      var Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)\.(png|jpg|gif|jpeg)?/;
-      var objExp = new RegExp(Expression);
-      if (objExp.test(str) == true) {
-        return true;
-      } else {
-        return false;
-      }
+      var reg = /\.(png|jpg|gif|jpeg)$/;
+     
+      return reg.test(str);
     }
   },
   watch: {
